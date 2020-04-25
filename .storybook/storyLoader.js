c9634ba5c23e4822b1264f7ba31561a354768327
Yanglin Zhao <yz1876@gmail.com>
2020-04-24 23:33:24 -0400

    Add hello world storybook

--- .storybook/storyLoader.js
+++ .storybook/storyLoader.js
@@ -4,11 +4,11 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-
+	require('../src/layout/Box.story');
 }
 
 const stories = [
-
+	'../src/layout/Box.story'
 ];
 
 module.exports = {
