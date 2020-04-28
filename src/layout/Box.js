5e0b5734f9d0c760b6852d7c58dce959763c5890
Yanglin Zhao <yz1876@gmail.com>
2020-04-27 22:10:45 -0400

    Implement responsive prop parsing

--- src/layout/Box.js
+++ src/layout/Box.js
@@ -1,14 +1,26 @@
 import React, { createRef } from "react";
 
 import { useLocalCssVariable } from "../lib/useCssVariable";
+import { parseResponsive } from "../lib/responsive";
 
 import styles from "./Box.module.css";
 
 function Box(props) {
   const padding = props.padding || 8;
+  const responsivePadding = parseResponsive(padding);
 
   const ref = createRef(null);
-  useLocalCssVariable(ref, "--ui-system-box", padding);
+  useLocalCssVariable(ref, "--ui-system-box-padding", responsivePadding.mobile);
+  useLocalCssVariable(
+    ref,
+    "--ui-system-box-padding-tablet",
+    responsivePadding.tablet
+  );
+  useLocalCssVariable(
+    ref,
+    "--ui-system-box-padding-desktop",
+    responsivePadding.desktop
+  );
 
   return (
     <div className={styles.Box} ref={ref}>
