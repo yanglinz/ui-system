e798b65a8a16d3f541896cf07e3b8949ffe15880
Yanglin Zhao <yz1876@gmail.com>
2020-04-28 21:27:51 -0400

    Implement storybook for Box

--- src/layout/Box.story.js
+++ src/layout/Box.story.js
@@ -5,7 +5,19 @@ import Outline from "../storybook/Outline";
 
 export const main = () => (
   <Outline>
-    <Box padding={8}>Hello Button</Box>
+    <Box padding={8}>Hello Box!</Box>
+  </Outline>
+);
+
+export const larger = () => (
+  <Outline>
+    <Box padding={32}>Hello Box!</Box>
+  </Outline>
+);
+
+export const responsive = () => (
+  <Outline>
+    <Box padding={(8, 16, 32)}>Hello Box!</Box>
   </Outline>
 );
 
