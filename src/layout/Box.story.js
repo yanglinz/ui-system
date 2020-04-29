0038bdfb74956aa1d51f6c93f6c16fa6408b51db
Yanglin Zhao <yz1876@gmail.com>
2020-04-28 21:25:33 -0400

    Implement placeholder and outline

--- src/layout/Box.story.js
+++ src/layout/Box.story.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 import Box from "./Box";
+import Outline from "../storybook/Outline";
 
-export const main = () => <Box padding={8}>Hello Button</Box>;
+export const main = () => (
+  <Outline>
+    <Box padding={8}>Hello Button</Box>
+  </Outline>
+);
 
 export default { title: "Box" };
--- src/storybook/Placeholder.js
+++ src/storybook/Placeholder.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-function Placeholder() {
-  return null;
+import styles from "./Placeholder.module.scss";
+
+function Placeholder(props) {
+  const width = props.width || "100%";
+  const height = props.height || 60;
+  const text = props.text || "Placeholder";
+  return (
+    <div className={styles.Placeholder} style={{ width, height }}>
+      {text}
+    </div>
+  );
 }
 
 export default Placeholder;
