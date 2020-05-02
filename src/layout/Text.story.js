64d17197a516195e5464c783d0d91832d68ffaea
Yanglin Zhao <yz1876@gmail.com>
2020-05-02 14:50:48 -0400

    Use the outline component

--- src/layout/Text.story.js
+++ src/layout/Text.story.js
@@ -1,25 +1,20 @@
 import React from "react";
 
 import Text from "./Text";
-import * as colors from "../theme/colors";
+import Outline from "../storybook/Outline";
 
 export const main = () => {
   let sizes = [4, 8, 10, 12, 14, 16, 18, 24, 32, 48];
   sizes = [].concat(sizes, sizes.slice().reverse());
 
   return (
-    <div
-      style={{
-        background: colors.PRIMARY_COLORS.c10,
-        border: `1px solid ${colors.PRIMARY_COLORS.c9}`,
-      }}
-    >
+    <Outline>
       {sizes.map((s, i) => (
         <Text key={i} size={s}>
           Hello design system!
         </Text>
       ))}
-    </div>
+    </Outline>
   );
 };
 
