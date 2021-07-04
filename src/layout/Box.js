490ca4c4f83a1fe986365e91cda1e4dccf29e56f
Yanglin Zhao <yz1876@gmail.com>
2021-07-03 20:31:54 -0400

    Add vanilla extract to next js (#6)

--- src/layout/Box.js
+++ src/layout/Box.js
@@ -3,21 +3,17 @@ import React, { createRef } from "react";
 import { useLocalCssVariable } from "../lib/useCssVariable";
 import { parseResponsive } from "../lib/responsive";
 
-import styles from "./Box.module.css";
+import { boxStyle } from "./Box.css.ts";
 
 export function Box(props) {
   const padding = parseResponsive(props.padding || 8);
 
-  const ref = createRef(null);
-  useLocalCssVariable(ref, "--ui-system-box-padding", padding.mobile);
-  useLocalCssVariable(ref, "--ui-system-box-padding-tablet", padding.tablet);
-  useLocalCssVariable(ref, "--ui-system-box-padding-desktop", padding.desktop);
+  // const ref = createRef(null);
+  // useLocalCssVariable(ref, "--ui-system-box-padding", padding.mobile);
+  // useLocalCssVariable(ref, "--ui-system-box-padding-tablet", padding.tablet);
+  // useLocalCssVariable(ref, "--ui-system-box-padding-desktop", padding.desktop);
 
-  return (
-    <div className={styles.Box} ref={ref}>
-      {props.children}
-    </div>
-  );
+  return <div className={boxStyle}>{props.children}</div>;
 }
 
 export default Box;
