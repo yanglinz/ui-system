249ab288b4d01e1d653da82ee3ddb5227a218759
Yanglin Zhao <yz1876@gmail.com>
2021-07-03 20:13:32 -0400

    Use next-js instead of react-scripts (#1)
    * Upgrade node and yarn
    
    * Install next.js
    
    * Get next.js config working
    
    * Try without react scripts
    
    * Add next
    
    * Remove unused index.html
    
    * Use empty next.js config

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const withPrefresh = require("@prefresh/next");
+const preact = require("preact");
+const withPreact = require("next-plugin-preact");
+
+module.exports = withPreact({
+  // Empty next.js config
+});
