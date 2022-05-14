1c335d10589c49a9c142cac1b7f61a6126cefca1
Yanglin Zhao <hi@yanglinzhao.com>
2022-05-13 21:55:28 -0400

    Add story

--- packages/preview/src/app.jsx
+++ packages/preview/src/app.jsx
@@ -1,13 +1,21 @@
+import { useState } from "preact/hooks";
+
 import LayerProvider from "../../preact-layers/src/LayerProvider";
 import Diaglog from "../../preact-composite/src/Dialog";
+
+function DialogStory() {
+  const [showDialog, setShowDialog] = useState(false);
+  const open = () => setShowDialog(true);
+  const close = () => setShowDialog(false);
+
+  return <Diaglog isOpen={showDialog}>Hello world!</Diaglog>;
+}
 
 export function App() {
   return (
     <>
       <LayerProvider>
-        <Diaglog>Hello world!</Diaglog>
-
-        <p>Hello world!</p>
+        <DialogStory />
       </LayerProvider>
     </>
   );
