5fcaa4dd714282a3ecfaa14d7fad2c952b145221
Yanglin Zhao <hi@yanglinzhao.com>
2022-05-14 11:28:29 -0400

    Add initial API for dialog component (#23)
    * Remove index.css
    
    * Remove index.css
    
    * Add layer components
    
    * Remove unused files
    
    * Add src
    
    * Progress
    
    * Add story
    
    * Progress on the API
    
    * Add dialog attributes
    
    * Progress
    
    * Try to use portal
    
    * Debug progress
    
    * Use default compat portal
    
    * Working implementation
    
    * Remove unused packages
    
    * Rename packages
    
    * Use the correct workspace import
    
    * Remove preact-layout
    
    * Rename to primitive
    
    * Nest src inside legacy
    
    * Don't use packages
    
    * Progress
    
    * Progress
    
    * Install
    
    * Fix imports
    
    * Fix imports
    
    * Add dist

---
+++ src/preview/src/app.jsx
@@ -0,0 +1,29 @@
+import { useState } from "preact/hooks";
+
+import LayerProvider from "../../preact-primitive/LayerProvider";
+import Diaglog from "../../preact-composite/Dialog";
+
+function DialogStory() {
+  const [showDialog, setShowDialog] = useState(false);
+  const open = () => setShowDialog(true);
+  const close = () => setShowDialog(false);
+
+  return (
+    <>
+      <button onClick={open}>Show Dialog</button>
+      <Diaglog isOpen={showDialog}>
+        <p>Hello there. I am a dialog</p>
+      </Diaglog>
+    </>
+  );
+}
+
+export function App() {
+  return (
+    <>
+      <LayerProvider mount={document.getElementById("app-layers")}>
+        <DialogStory />
+      </LayerProvider>
+    </>
+  );
+}
