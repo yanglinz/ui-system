5076779a0c156610a1820e54564271bf8eb80a3f
Yanglin Zhao <hi@yanglinzhao.com>
2023-10-06 20:10:30 -0400

    Style dialog overlay (#29)
    * Style dialog overlay
    
    * Style dialog content

--- src/Dialog.jsx
+++ src/Dialog.jsx
@@ -1,5 +1,10 @@
 import Layer, { useCurrentLayer } from "./Layer";
 
+function DialogOverlay(props) {
+  const layer = useCurrentLayer();
+  return <div className="fixed inset-0 bg-stone-300/50">{props.children}</div>;
+}
+
 function Dialog(props) {
   const { isOpen } = props;
   if (!isOpen) {
@@ -8,9 +13,7 @@ function Dialog(props) {
 
   return (
     <Layer>
-      <div role="dialog" aria-modal>
-        {props.children}
-      </div>
+      <DialogOverlay>{props.children}</DialogOverlay>
     </Layer>
   );
 }
