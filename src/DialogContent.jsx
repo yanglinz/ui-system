e130ed064f2dadb3162820f8f40569f51de5d74c
Yanglin Zhao <hi@yanglinzhao.com>
2023-10-08 19:57:46 -0400

    Implement outside click handler (#32)

--- src/DialogContent.jsx
+++ src/DialogContent.jsx
@@ -1,12 +1,14 @@
 import useRestoreFocus from "./useRestoreFocus";
+import useOutsideClick from "./useOutsideClick";
 
 function DialogContent(props) {
   const { onClose } = props;
 
   useRestoreFocus();
+  const ref = useOutsideClick(onClose);
 
   return (
-    <div role="dialog" aria-modal className="w-8/12 mx-auto mt-16">
+    <div ref={ref} role="dialog" aria-modal className="w-8/12 mx-auto mt-16">
       <div className="relative bg-white p-6">
         <button
           className="absolute top-0 right-6 bg-stone-200 px-1 text-blue-400 bold"
