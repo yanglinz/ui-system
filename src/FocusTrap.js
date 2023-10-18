37b310889d0cf67d4e83cc0d889955c1819d3407
Yanglin Zhao <hi@yanglinzhao.com>
2023-10-17 20:52:20 -0400

    Implement focus trapping (#33)

---
+++ src/FocusTrap.js
@@ -0,0 +1,13 @@
+import { useFocusTrap } from "@mantine/hooks";
+
+export default function FocusTrap(props) {
+  const ref = useFocusTrap();
+
+  // TODO: Conditionally trap focus based on whether current layer
+  // is the top layer Get the top layer and only trap focus there.
+  return (
+    <div className="--layer-trap-focus" ref={ref}>
+      {props.children}
+    </div>
+  );
+}
