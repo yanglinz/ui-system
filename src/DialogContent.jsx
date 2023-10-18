bee48b9f2af739b49450b38b554849dec4c2a892
Yanglin Zhao <hi@yanglinzhao.com>
2023-10-17 20:48:47 -0400

    Implement focus trapping

--- src/DialogContent.jsx
+++ src/DialogContent.jsx
@@ -1,5 +1,6 @@
 import useRestoreFocus from "./useRestoreFocus";
 import useOutsideClick from "./useOutsideClick";
+import FocusTrap from "./FocusTrap";
 
 function DialogContent(props) {
   const { onClose } = props;
@@ -9,16 +10,18 @@ function DialogContent(props) {
 
   return (
     <div ref={ref} role="dialog" aria-modal className="w-8/12 mx-auto mt-16">
-      <div className="relative bg-white p-6">
-        <button
-          className="absolute top-0 right-6 bg-stone-200 px-1 text-blue-400 bold"
-          onClick={onClose}
-        >
-          {/* TODO: Visually hidden close text */}
-          <span aria-hidden>×</span>
-        </button>
-        {props.children}
-      </div>
+      <FocusTrap>
+        <div className="relative bg-white p-6">
+          <button
+            className="absolute top-0 right-6 bg-stone-200 px-1 text-blue-400 bold"
+            onClick={onClose}
+          >
+            {/* TODO: Implement a visually hidden component */}
+            <span aria-hidden>×</span>
+          </button>
+          {props.children}
+        </div>
+      </FocusTrap>
     </div>
   );
 }
